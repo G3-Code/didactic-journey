ec162056700c8be392e67ce5329fa447e561276d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-06 14:44:05 -0500

    The project is set up to be worked on

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="container">App</div>;
 }
 
 export default App;
