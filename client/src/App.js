b279b5b13122324e6639046dcc41fb73be637a9a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-19 15:46:12 -0500

    Added the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
+import Header from "./components/Header";
+import { Route } from "react-router-dom";
+import AddProduct from "./components/AddProduct";
+import ListProduct from "./components/ListProduct";
+import SearchProduct from "./components/SearchProduct";
 
 function App() {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <Header />
+      <Route path="/addProduct" render={props => <AddProduct {...props} />} />
+      <Route path="/listProduct" render={props => <ListProduct {...props} />} />
+      <Route
+        path="/searchProduct"
+        render={props => <SearchProduct {...props} />}
+      />
+    </div>
+  );
 }
 
 export default App;
