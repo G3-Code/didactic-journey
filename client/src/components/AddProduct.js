c7912a158739adc210ec01f9c5f9789549de57dd
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-22 19:26:07 -0500

    Formatting the add product page

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddProduct() {
-  return <div>AddProduct</div>;
+  const [productName, setProductName] = useState("");
+
+  const handleChange = e => {
+    e.persist();
+    switch (e.target.name) {
+      case "productName": {
+        setProductName(e.target.value);
+        break;
+      }
+      default: {
+        console.log(e.target.name);
+      }
+    }
+  };
+  return (
+    <div className="container">
+      <div className="row center-align">
+        <div className="col s12 l4 m6 card offset-l4">
+          <h4 className="teal-text">Add Product</h4>
+          <form>
+            <input
+              type="text"
+              placeholder="Product Name"
+              value={productName}
+              name="productName"
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
