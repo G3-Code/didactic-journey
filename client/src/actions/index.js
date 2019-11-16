c132095b6dc180c65ade684b301300e90899fbd2
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-11-16 09:21:17 -0600

    Added action method for add product

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,1 +1,29 @@
-export const TEMP = "TEMP";
+import axios from "axios";
+export const ADD_PRODUCT_SUCCESS = "ADD_PRODUCT_SUCCESS";
+export const ADD_PRODUCT_START = "ADD_PRODUCT_START";
+export const ADD_PRODUCT_FAILURE = "ADD_PRODUCT_FAILURE";
+
+const APIURL = "http://localhost:4000/";
+export const addProduct = product => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: ADD_PRODUCT_SUCCESS,
+        payload: ""
+      });
+
+      const products = await axios
+        .post(`${APIURL}product`, product)
+        .then(res => res.data);
+      dispatch({
+        type: ADD_PRODUCT_SUCCESS,
+        payload: products
+      });
+    } catch (error) {
+      dispatch({
+        type: ADD_PRODUCT_FAILURE,
+        payload: error
+      });
+    }
+  };
+};
--- server.js
+++ server.js
@@ -14,6 +14,10 @@ server.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
 });
 
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Retail server is up and running!" });
+});
+
 server.post("/product", (req, res) => {
   console.log(req.body);
   products.push(req.body);
